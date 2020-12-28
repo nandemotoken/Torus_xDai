cf2411d3ef1b14e4e904533f5e8eae671bb62091
nandemotoken <46263082+nandemotoken@users.noreply.github.com>
2020-12-28 16:56:20 +0900

    Update EnableTorus_xDai.js

--- EnableTorus_xDai.js
+++ EnableTorus_xDai.js
@@ -20,8 +20,14 @@ await torus.login(); // await torus.ethereum.enable()
 web3tr = new Web3(torus.provider);
 }
 
+const Address = "0xA466528252Eb9aE675d5E6C63163CAACB5E47DC6";
+
 async function checkmycake(){
   console.log("test");
+  
+  mycontract = await new web3js.eth.Contract(abi, Address);
+  let fromblockchain1 = await mycontract.methods.name().call();
+  console.log(fromblockchain1);
 }
 
 newTorus();
