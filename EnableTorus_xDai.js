c596fe354ac2249df1127333de2fac53e4329faf
nandemotoken <46263082+nandemotoken@users.noreply.github.com>
2020-12-28 11:22:19 +0900

    Create EnableTorus_xDai.js

---
+++ EnableTorus_xDai.js
@@ -0,0 +1,22 @@
+let web3tr;
+
+async function newTorus() {
+
+const torus = new Torus({
+  buttonPosition: "top-right" // default: bottom-left
+});
+await torus.init({
+  buildEnv: "production", // default: production
+  enableLogging: true, // default: false
+  network: {
+    host: "matic", // default: mainnet
+    chainId: 137, // default: 1
+    networkName: "Matic Mainnet" // default: Main Ethereum Network
+  },
+  showTorusButton: true // default: true
+});
+await torus.login(); // await torus.ethereum.enable()
+web3tr = new Web3(torus.provider);
+}
+
+newTorus();
